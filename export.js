be7df418864cea3b0ae5c8fe739ca2dc204d431b
Yasar icli <hello@yasaricli.com>
2014-12-27 20:41:33 +0200

    removed export.js and version updated

--- export.js
+++
@@ -1,3 +0,0 @@
-// global on the window (or global) object, while Meteor expects a file-scoped global variable
-slugify = this.slugify;
-delete this.slugify;
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'yasaricli:slugify',
   summary: 'Remove all special characters from a string.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/yasaricli/meteor-slugify.git'
 });
 
